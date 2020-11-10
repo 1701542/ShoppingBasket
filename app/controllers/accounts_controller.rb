a6bcd98ab56040357996ce3706af534ac1b961ca
Dominik <uadfrankowski@gmail.com>
2020-11-10 12:34:26 +0000

    Added products, accounts, and baskets to routes.rb

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,31 @@
 class AccountsController < ApplicationController
+
+	def index
+		@account = Account.all
+	end
+	
+	def show
+	
+	end
+	
+	def new
+	
+	end
+
+	def edit
+	
+	end
+	
+	def create
+	
+	end
+	
+	def update
+	
+	end
+	
+	def destroy
+	
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+	get 'welcome/index'
   
-  root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resources :products
+	resources :accounts do
+		resources :baskets
+	end
+  
+	root 'welcome#index'
 end
