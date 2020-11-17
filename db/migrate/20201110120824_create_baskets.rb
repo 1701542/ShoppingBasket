c3906fa5a91b5850249a763447dd875b8344c2e2
Dominik <uadfrankowski@gmail.com>
2020-11-17 12:59:12 +0000

    rails d scaffold unneeded files and changed decrease logic in product show

--- db/migrate/20201110120824_create_baskets.rb
+++
@@ -1,9 +0,0 @@
-class CreateBaskets < ActiveRecord::Migration[6.0]
-  def change
-    create_table :baskets do |t|
-      t.references :product, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
